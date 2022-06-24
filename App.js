1175004200e4118107876bbbd83a3086c29c03d2
sasi1278 <sasi.chimmili@gmail.com>
2022-06-24 22:19:34 +0530

    e

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import React, {useEffect} from 'react';
+import {View, Text} from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
 
-const App=()=>{
-  useEffect(()=>{
-    SplashScreen.hide()
-  })
-}
+const App = () => {
+  useEffect(() => {
+    setTimeout(() => {
+      SplashScreen.hide();
+    }, 10000);
+  });
+  return (
+    <View>
+      <Text>sasi</Text>
+    </View>
+  );
+};
 
 export default App;
