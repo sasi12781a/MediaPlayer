d77ebd51729e8b0241c1c27e6c9366521ea5041d
sasi1278 <sasi.chimmili@gmail.com>
2022-06-29 14:58:53 +0530

    j

--- Src/HomeStack/Video/videoPlayer.js
+++ Src/HomeStack/Video/videoPlayer.js
@@ -1,58 +1,154 @@
-/* eslint-disable no-undef */
-import React, {useState} from 'react';
-import {View, StyleSheet} from 'react-native';
+/* eslint-disable prettier/prettier */
+import React, {useState, useRef} from 'react';
+import { StyleSheet, View, Platform, Dimensions } from 'react-native';
+import MediaControls, { PLAYER_STATES } from 'react-native-media-controls';
 import Video from 'react-native-video';
-//import { Dimensions } from 'react-native';
-// import { DATA } from './AudioList';
+import Orientation from 'react-native-orientation';
 
-const PlayVideo = ({route}) => {
-  const [] = useState(true);
 
-  const data = route.params.data;
-  console.log('....', data);
-  console.log('playVideo', data.video);
+const screenHeight = Dimensions.get('screen').height;
+const screenWidth = Dimensions.get('screen').width;
 
-  //require('../../asserts/video.mp4')  ${data.song} ${data.song}`
+const PlayVideo = ({navigation,route}) => {
 
-  // const isPortrait = () => {
-  //   const dim = Dimensions.get('screen');
-  //   return dim.height >= dim.width;
-  // };
+    // We will use this hook to get video current time and change it throw the player bar.
+    const videoPlayer = useRef(null);
+    // eslint-disable-next-line semi
+    const data = route.params.data
+    /**
+     * The following useState hooks are created to control the vide duration, if the video
+     * is paused or not, the current time video, if the player is PLAYING/PAUSED/ENDED and if the video
+     * is loading.
+     */
+    const [duration, setDuration] = useState(0);
+    const [paused, setPaused] = useState(true);
 
-  // const isLandscape = () => {
-  //   const dim = Dimensions.get('screen');
-  //   return dim.width >= dim.height;
-  // };
+    const [currentTime, setCurrentTime] = useState(0);
+    const [playerState, setPlayerState] = useState(PLAYER_STATES.PAUSED);
+    const [isLoading, setIsLoading] = useState(true);
 
-  // dimentions=() => {
-  //   setDimentions({
-  //       orientation: Platform.isPortrait() ? 'portrait' : 'landscape'
-  //   });
-  // }
+    // This function is triggered when the user released the player slider.
+    const onSeek = (seek) => {
+        videoPlayer?.current.seek(seek);
+    };
 
-  return (
-    <View style={styles.container}>
-      <Video
-        source={data.video}
+    // This function is triggered when the user interact with the player slider.
+    const onSeeking = (currentVideoTime) => setCurrentTime(currentVideoTime);
+
+    // This function is triggered when the play/pause button is pressed.
+    const onPaused = (newState) => {
+        setPaused(!paused);
+        setPlayerState(newState);
+    };
+
+    /**
+     * This function is triggered when the replay button is pressed.
+     * There is a minmial bug on Android devices that does not allow the player to replay the video if changing the state to PLAYING, so we have to use the 'Platform' to fix that.
+     */
+    const onReplay = () => {
+        videoPlayer?.current.seek(0);
+        setCurrentTime(0);
+        if (Platform.OS === 'android') {
+            setPlayerState(PLAYER_STATES.PAUSED);
+            setPaused(true);
+        } else {
+            setPlayerState(PLAYER_STATES.PLAYING);
+            setPaused(false);
+        }
+    };
+
+    // This function is triggered while the video is playing.
+    // eslint-disable-next-line no-shadow
+    const onProgress = (data) => {
+        if (!isLoading) {
+            setCurrentTime(data.currentTime);
+        }
+    };
+
+    /**
+     * This function and the next one allow us doing something while the video is loading.
+     * For example we could set a preview image while this is happening.
+     */
+    // eslint-disable-next-line no-shadow
+    const onLoad = (data) => {
+        setDuration(Math.round(data.duration));
+        setIsLoading(false);
+    };
+
+    const onLoadStart = () => setIsLoading(true);
+
+    // This function is triggered when the player reaches the end of the media.
+    const onEnd = () => {
+        setPlayerState(PLAYER_STATES.ENDED);
+        setCurrentTime(duration);
+    };
+
+    // useState hook to check if the video player is on fullscreen mode
+
+    const [isFullScreen, setIsFullScreen] = useState(false);
+
+    // This function is triggered when the user press on the fullscreen button or to come back from the fullscreen mode.
+    const onFullScreen = () => {
+        if (!isFullScreen) {
+          Orientation.lockToLandscape();
+        } else {
+          Orientation.lockToPortrait();
+        }
+        setIsFullScreen(!isFullScreen);
+    };
+
+    return (
         // eslint-disable-next-line react-native/no-inline-styles
-        style={{height: '100%', width: '100%'}}
-        controls={true}
-        audioOnly={true}
-        resizeMode="contain"
-      />
-    </View>
-  );
-};
-export default PlayVideo;
+        <View style={{ marginHorizontal: isFullScreen ? 50 : 0 , flex:1, justifyContent:'center', alignItems:'center'}}>
+            <Video
+                onEnd={onEnd}
+                onLoad={onLoad}
+                onLoadStart={onLoadStart}
+                posterResizeMode={'cover'}
+                onProgress={onProgress}
+                paused={paused}
+                ref={(ref) => (videoPlayer.current = ref)}
+                resizeMode={'cover'}
+                source={data.video}
+                style={styles.backgroundVideo}
+                //controls={true}
+            />
+            <MediaControls
+                isFullScreen={isFullScreen}
+                duration={duration}
+                isLoading={isLoading}
+                progress={currentTime}
+                onFullScreen={onFullScreen}
+                onPaused={onPaused}
+                onReplay={onReplay}
+                onSeek={onSeek}
+                onSeeking={onSeeking}
+                mainColor={'green'}
+                playerState={playerState}
+                style={isFullScreen ? styles.backgroundVideoFullScreen : styles.backgroundVideo}
+                sliderStyle={isFullScreen ? { containerStyle: styles.mediaControls, thumbStyle: {}, trackStyle: {} } : { containerStyle: {}, thumbStyle: {}, trackStyle: {} }}
+            />
+        </View>
 
-styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  logoIcon: {
-    width: 300,
-    height: 300,
-  },
+    );
+};
+
+const styles = StyleSheet.create({
+    backgroundVideo: {
+        height: 250,
+        width: '100%',
+    },
+    mediaControls: {
+        width: screenHeight - 170,
+        height: '100%',
+        flex: 1,
+        alignSelf: Platform.OS === 'android' ? screenHeight < 800 ? 'center' : 'flex-start' : 'center',
+    },
+    backgroundVideoFullScreen: {
+        height: screenHeight,
+        width: screenWidth,
+    },
 });
+
+// eslint-disable-next-line eol-last
+export default PlayVideo;
